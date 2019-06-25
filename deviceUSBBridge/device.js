ba4c979d972346e5fb6e431e98a7570efd61f73d
Ryan Crawford <RyanCCrawford@live.com>
2019-06-25 15:59:41 -0400

    Changed Timmings

--- deviceUSBBridge/device.js
+++ deviceUSBBridge/device.js
@@ -1,33 +1,69 @@
-var five = require("johnny-five")
+const five = require("johnny-five");
+const request = require('request');
 
 
-
-function J5(cb) {
-    var bord = this
-    this.lightVal = 0.00
-    this.soilVal = 0.00
-    this.tempVal 
-    board = new five.Board({ port: "COM8" })
-   
+function J5(confg, cb) {
+    
+    this.lightVal = 0
+    this.soilVal = 0
+    this.tempVal = 0 
+    this.config = confg
+    this.serviceURL = 'http://' + this.config[0].REMOTE_SERVER + ':' + this.config[1].REMOTE_SERVER_PORT
+    this.interval = parseInt(this.config[4].TIME_INTERVAL)
     this.setLight = function(val) {
         this.lightVal = val
-        draw()
+        
     }
     this.setTemp = function(val) {
         this.tempVal = val
-        draw()
+        
     }
     this.setSoil = function(val) {
        this.soilVal = val
-        draw()
+       
     }
-    draw = function(){
+    this.draw = function(){
         console.clear()
-        console.log("Light:" + this.lightVal)
-        console.log("Soil:" + this.soilVal)
-        console.log("Temp:" + this.tempVal)
+        console.log("Light:" + bord.lightVal)
+        console.log("Soil:" + bord.soilVal)
+        console.log("Temp:" + bord.tempVal)
 
     }
+    this.timer = function () {
+      setInterval(this.loop, this.interval);
+    }
+   this.loop = function () {
+      
+    
+       var data = { temp: bord.tempVal, soil: bord.soilVal, light: bord.lightVal }
+     return  request.post({ uri: bord.serviceURL, multipart: [{ 'content-type': 'application/json', body: JSON.stringify(data) }] }, function (error, response, body) {
+            if (error) {
+                console.log('error:', error); // Print the error if one occurred
+            }
+         if (response && response.statusCode) {
+             console.log(response); // Print the response status code if a response was received
+
+             if (body) {
+                 var commands = JSON.parse(body)
+                 if (commands.relay) {
+                     bord.relay(commands.relay)
+                 }
+                 if (commands.led) {
+                     bord.led(commands.led)
+                 }
+                 if (commands.lcd) {
+                     bord.lcd(commands.lcd)
+                 }
+             }
+         }
+        }
+        )
+    }
+    var bord = this;
+    board = new five.Board({ port: "COM8" })
+
+    setInterval(this.draw, 2000)
+
     board.on('ready', function () {
 
        
@@ -39,30 +75,42 @@ function J5(cb) {
             pin: "A2"
         })
         
-        var relay = new five.Relay({
-            pin: 2
-        });
+     //   var relay = new five.Relay({
+    //        pin: 2
+    //    });
+     //   function r(state) {
+         //   if (state === "open") {
+              //  relay.open();
+         //   } else {
+               // relay.close();
+          //  }
+     //   }
+
         photoresistor.on("data", function () {
-            console.log("The Light Sensor data: " + this.value);
             bord.setLight(this.value)
         })
         soilMoisture.on("data", function () {
-            console.log("Soil Moisture data: " + this.value);
             if (this.value > 250) {
-                relay.close();
+               // r("close");
             } else {
-                relay.open();
+              //  r("open");
             }
             bord.setSoil(this.value)
         })
         tempsensor.on("data", function () {
-            console.log("Temp data: " + this.value);
-            bord.setTemp(this)
+            bord.setTemp(this.F/2)
         })
 
-        cb(this)
-
+      
+        cb()
+        
+      
     })
+    
+
 }
+
+
+
 
 module.exports = J5
--- deviceUSBBridge/server.js
+++ deviceUSBBridge/server.js
@@ -1,38 +1,22 @@
 
 const express = require("express")
+const SF = require(__dirname + "/settingsFile.js")
+const J5 = require(__dirname + "/device.js")
 var app = express()
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 var Configs = {}
-const SF = require(__dirname + "/settingsFile.js")
-const J5 = require(__dirname + "/device.js")
-const RES = require(__dirname + "/client.js")
-
-
-
 function set(settings) {
     Configs = settings
     console.log(Configs)
     var PORT = process.env.PORT || Configs[3].THIS_PORT
-
     app.listen(PORT, function () {
         console.log("Server listening on PORT: " + PORT)
-        J5(loop)
+        var j = new J5(Configs, function () {
+            j.timer()
+        });
     })
-    
 }
-
-
-function loop(j5obj) {
-    
-    setInterval(function () {
-        console.log(Configs[1].REMOTE_SERVER_PORT)
-        var mac = 'http://' + Configs[0].REMOTE_SERVER + ':' + Configs[1].REMOTE_SERVER_PORT
-        RES(J5, mac)
-    }, 5000);
-
-}
-
 SF(set)
 
